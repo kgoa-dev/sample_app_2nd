cfac6e8118ab20b74032e93c90226d1eaba5ae34
kgoa <y-keigoe@ruby-dev.jp>
2022-07-25 11:46:41 +0900

    Use SSL and the Puma webserver in production

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,4 +13,44 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  describe 'POST /users #create' do
+    it '無効な値だと登録されないこと' do
+      # 波括弧で囲った部分の処理を実行する前後での変化の有無を検証するテスト
+      expect{
+        # users_pathにpostリクエストを送信する前後で
+        post users_path, params: { user: { name: "",
+                                          email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
+        }.to_not change(User, :count)
+        # Userモデルの数(User.all.count)に差異が生じるかどうか
+        # 差異が発生→無効な値で登録がされている→NG
+    end
+  end
+
+  context '有効な値の場合' do
+    let(:user_params) { { user: { name: 'Example User',
+                                  email: 'user@example.com',
+                                  password: 'password',
+                                  password_confirmation: 'password' } } }
+
+    it '登録されること' do
+      expect {
+        post users_path, params: user_params
+      }.to change(User, :count).by 1
+      # 1件増加しているか
+    end
+
+    it 'users/showにリダイレクトされること' do
+      post users_path, params: user_params
+      user = User.last
+      expect(response).to redirect_to user
+      # post users_path後にリダイレクトが行われるかどうか
+    end
+
+    it 'flashが表示されること' do
+      post users_path, params: user_params
+      expect(flash).to be_any
+    end
+  end
 end
---
+++ spec/system/user_specs_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "UserSpecs", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe '#create' do
+    context '無効な値の場合' do
+      it '' do
+        visit signup_path
+        # fill_inは指定のフォームにwith:で指定した文字列を入力するメソッド
+        fill_in 'Name', with: ''
+        fill_in 'Email', with: 'user@invlid'
+        fill_in 'Password', with: 'foo'
+        fill_in 'Confirmation', with: 'bar'
+        click_button 'Create my account'
+
+        # have_selector = 特定の要素がページ上に存在するか
+        expect(page).to have_selector 'div#error_explanation'
+        expect(page).to have_selector 'div.field_with_errors'
+        # エラーメッセージを表示しているdivタグの有無を検証
+      end
+    end
+  end
+end
