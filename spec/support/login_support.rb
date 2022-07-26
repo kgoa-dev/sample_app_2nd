71dcacbc266dfe6abb171b90993f6eab66dccfeb
kgoa <y-keigoe@ruby-dev.jp>
2022-07-26 13:36:36 +0900

    Implement advanced login

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -9,6 +9,12 @@ module LoginSupport
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
 end
 
 RSpec.configure do |config|
