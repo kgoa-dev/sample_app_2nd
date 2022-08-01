33ade4e02a0bf591e79ee124219be328735fe4cb
kgoa <y-keigoe@ruby-dev.jp>
2022-08-01 15:40:21 +0900

    Add password reset

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+  let(:user) { FactoryBot.create(:user) }
+
   describe "account_activation" do
-    let(:user) { FactoryBot.create(:user) }
     let(:mail) { UserMailer.account_activation(user) }
 
     before do
@@ -34,4 +35,31 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
+  describe 'password_reset' do
+    let(:mail) { UserMailer.password_reset(user) }
+
+    before do
+      user.reset_token = User.new_token
+    end
+
+    it '"Password reset"というタイトルで送信されること' do
+      expect(mail.subject).to eq('Password reset')
+    end
+
+    it '送信先が"to@example.org"であること' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it '送信元が"from@example.com"であること' do
+      expect(mail.from).to eq(['noreply@example.com'])
+    end
+
+    it 'メール本文にreset_tokenが表示されていること' do
+      expect(mail.body.encoded).to match(user.reset_token)
+    end
+
+    it 'メール本文にユーザのemailが表示されていること' do
+      expect(mail.body.encoded).to match(CGI.escape(user.email))
+    end
+  end
 end
